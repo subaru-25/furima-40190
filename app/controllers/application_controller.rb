f58a9c1efa0f27287695b3a6d0ded4978f58e38e
subaru-25 <misuzu101025@gmail.com>
2024-04-09 21:05:02 +0900

    差分

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :birth_date])
@@ -11,5 +12,6 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'yamagata' && password == '8792'
   end
+  
  end
 end
