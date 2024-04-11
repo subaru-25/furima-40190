93fc4984cb21cc41ec6772fd869a3841000a461a
subaru-25 <misuzu101025@gmail.com>
2024-04-11 22:49:44 +0900

    spec/factories/users.rbの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do 
   factory :user do 
     nickname { "test" }
-    email { "test@example.com" }
+    email { Faker::Internet.unique.email }
     password { "a123456" }
     password_confirmation { "a123456" }
     last_name { "田中" }
