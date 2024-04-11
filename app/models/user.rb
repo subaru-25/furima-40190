1a14003b651308fb702a4fd4968d537fd9f1432a
subaru-25 <misuzu101025@gmail.com>
2024-04-11 22:46:14 +0900

    models/user.rbの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,10 @@ class User < ApplicationRecord
          validates :nickname, presence: true
          validates :birth_date, presence: true
        
-         validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/, message: 'には@を含む必要があります' }  
-         validates :password, presence: true, length: {minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, message: 'は6文字以上かつ半角英数字混合で入力してください' }, confirmation: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, message: 'is invalid. Input half-width characters.' }, confirmation: true
        
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください'}
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角（漢字・ぁ-んァ-ン一-龥）で入力してください'}
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください'}
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ぁ-んァ-ン一-龥）で入力してください'}
        
          validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）で入力してください'}
          validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）で入力してください'}
