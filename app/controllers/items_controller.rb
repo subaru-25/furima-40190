c297bef547bd0c37610bb56f2785daea2699f112
subaru-25 <misuzu101025@gmail.com>
2024-04-14 00:41:49 +0900

    一旦セーブ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path # 成功したらトップページにリダイレクト
+    else
+      render :new # 失敗したらnewアクションを再度呼び出す
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_image, :item_name, :item_info, :item_price, :shipping_fee_burden, :category, :condition, :prefecture, :shipping_time)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :item_image
+  validates :item_image, presence: true
   validates :item_name, presence: true
+  validates :item_info, presence: true
   validates :item_price, presence: true
   validates :shipping_fee_burden, presence: true
+  validates :category, presence: true
+  validates :condition, presence: true
+  validates :prefecture, presence: true
+  validates :shipping_time, presence: true
 end
