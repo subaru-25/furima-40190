4d8fcfdda82d39f6ce69220015b5d46a8996eb95
subaru-25 <misuzu101025@gmail.com>
2024-04-14 16:22:54 +0900

    商品情報がログインしていたら表示できるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create] 
+
   def index
+    @items = Item.all
   end
 
   def new
     @item = Item.new
   end
+
+  def show
+    @item = Item.find(params[:id])
+    end
 
   def create
     @item = Item.new(item_params)
