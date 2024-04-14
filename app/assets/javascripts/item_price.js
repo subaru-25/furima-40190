cdfae41ea44e96f811ffb1d4977efc352e56962a
subaru-25 <misuzu101025@gmail.com>
2024-04-14 17:24:42 +0900

    販売手数料と販売利益のJava設定。

---
+++ app/assets/javascripts/item_price.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitNum = document.getElementById("profit");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1); // 手数料を10%とする場合
+    profitNum.innerHTML = Math.floor(inputValue * 0.9); // 手数料が10%なら利益は90%
+  })
+})
