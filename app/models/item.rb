f1122a41a4f47dfc55e3e0ea7d63766e08dcf8c9
subaru-25 <misuzu101025@gmail.com>
2024-04-14 16:34:47 +0900

    価格のバリテーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :item_name, presence: true
   validates :item_info, presence: true
-  validates :item_price, presence: true
+  validates :item_price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/, message: "半角数値のみ使用可能です" }
   validates :shipping_fee_burden, presence: true
   validates :category, presence: true
   validates :condition, presence: true
