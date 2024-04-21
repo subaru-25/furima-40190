0973361b4bf772a012bd82fcbf92305ac2059bb0
subaru-25 <misuzu101025@gmail.com>
2024-04-21 15:44:42 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create] 
+  before_action :authenticate_user!, only: [:new, :create, :edit]  
 
   def index
     @items = Item.order(created_at: :desc) 
@@ -23,9 +23,26 @@ class ItemsController < ApplicationController
       end
     end
 
+    def edit
+      @item = Item.find(params[:id])
+      unless current_user.id == @item.user_id
+        redirect_to root_path
+      end
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to item_path(@item) # 編集が完了したら該当商品の詳細ページに遷移
+      else
+        logger.debug(@item.errors.full_messages)
+        render :edit, status: :unprocessable_entity # 編集に失敗したら、再度編集画面を表示
+      end
+    end
+
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :item_info, :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id, :shipping_time_id, :item_price)
+    params.require(:item).permit(:item_name, :item_info, :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id, :shipping_time_id, :item_price, :image)
   end
 end
