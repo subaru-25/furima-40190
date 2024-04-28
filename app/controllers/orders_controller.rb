bef47aac9b2dd205a4e39b6db0145a8ff0c851db
subaru-25 <misuzu101025@gmail.com>
2024-04-28 22:27:33 +0900

    エラー表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
       @order_form.save
       return redirect_to root_path
     else
-      render :index
+      render :index, status: :unprocessable_entity
     end
 end
 
