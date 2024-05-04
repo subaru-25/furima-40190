7f2831e9fa5726065c24cd07846e64124ff85a39
subaru-25 <misuzu101025@gmail.com>
2024-05-04 15:52:26 +0900

    ブラウザのカード情報を取得する

---
+++ app/javascript/order.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+  const cardNumber = document.getElementById("number-form");
+  console.log(cardNumber);   // cardNumberの出力
+
+  const cardExpiry = document.getElementById("expiry-form");
+  console.log(cardExpiry);   // cardExpiryの出力
+
+  const cardCvc = document.getElementById("cvc-form");
+  console.log(cardCvc);      // cardCvcの出力
+});
