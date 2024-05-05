702212600fc01d90012ff3eca98c7bbf96ff9956
subaru-25 <misuzu101025@gmail.com>
2024-05-05 22:36:46 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,8 +5,8 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:migrate
+
 
 # # addressesテーブルの削除
 # bundle exec rails generate migration DropAddressesTable
@@ -17,5 +17,5 @@ DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 # bundle exec rake db:migrate
 
 # # itemsテーブルの削除
-bundle exec rails generate migration DropItemsTable
-bundle exec rake db:migrate
+# bundle exec rails generate migration DropItemsTable
+# bundle exec rake db:migrate
