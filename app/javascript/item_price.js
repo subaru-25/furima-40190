093b1c5122ecd7da039faee4dd4d5a4fd61c061e
subaru-25 <misuzu101025@gmail.com>
2024-05-06 13:44:12 +0900

    カテゴリー/商品の状態/小数点以下を切り捨て

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,4 @@
+// document.addEventListener('turbolinks:load', () => { 
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
   const addTaxDom = document.getElementById("add-tax-price");
@@ -6,8 +7,9 @@ window.addEventListener('turbo:load', () => {
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
 
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1); // 手数料を10%とする場合
-    profitNum.innerHTML = Math.floor(inputValue * 0.9); // 手数料が10%なら利益は90%
+    let fee = Math.floor(inputValue * 0.1); // 手数料を10%とする場合
+    addTaxDom.innerHTML = fee; 
+    profitNum.innerHTML = inputValue - fee; // 利益は入力金額から手数料を差し引いたもの
   })
 });
 
